7c282359bf088f399b0cc0484626e6fab2127966
Tiisu <tiisusharif@gmail.com>
2025-07-11 22:23:48 +0000

    worked on creating products

--- product-store/frontend/src/components/Navbar.jsx
+++ product-store/frontend/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { PlusIcon, SunIcon } from 'lucide-react'
+import { Link, Links } from 'react-router-dom'
 
 function Navbar() {
   return (
     // creating a navbar component with logo and create icon using lucide icons and a theme change button
     <div className='flex justify-between items-center p-4 bg-gray-800 text-white mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-      <div className='text-xl font-bold'>Product Store</div>
+      <Link to="/">
+            <div className='text-xl font-bold'>Product Store</div>
+      </Link>
+      
       <div className='flex items-center space-x-4'>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+      <Link to="/create-product">
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
           <PlusIcon className='inline-block mr-2' />
         </button>
+      
+
+      </Link>
         <button className='bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded'>
           <SunIcon className='inline-block mr-2' />
         </button>
--- product-store/frontend/src/pages/CreatePage.jsx
+++ product-store/frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,107 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const CreatePage = () => {
+  // This is the create page where we will create a new product
+  const navigate = useNavigate();
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    image: '',
+    stock: '',
+    description: ''
+
+    
+  });
+
+  // console.log(newProduct)
+
+  const submitProduct = async (e) => {
+    e.preventDefault()
+    const serializedData = {
+      name: newProduct.name,
+      price: Number(newProduct.price),
+      stock: Number(newProduct.stock),
+      Image: newProduct.image,
+      description: newProduct.description,
+    }
+    try {
+      const response = await fetch('http://localhost:3000/api/products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newProduct),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Product created:", data.product)
+        navigate("/")
+      }
+
+      
+    } catch (error) {
+      console.error('Error creating product:', error);
+    }
+  }
   return (
-    <div>CreatePage</div>
+    <>  
+      <div className='w-full border h-screen'>
+        <div className='p-5 mx-auto '>
+          <h1 className='text-2xl font-bold text-center'>Create New Product</h1>
+          <form onSubmit={submitProduct} action="POST" className='border p-5 rounded mt-5'>
+            <div>
+              <input type="text" placeholder='Enter the product Name' id='name' className='border w-full mb-4 p-2 rounded' 
+              value={newProduct.name}
+              onChange={(e) => setNewProduct ({...newProduct, name: e.target.value})}
+              />
+            </div>
+            <div>
+              <input type="number" placeholder='Enter the product price' id='price' className='border w-full mb-4 p-2' min={0}
+              value={newProduct.price}
+              onChange={(e) => setNewProduct ({...newProduct, price: e.target.value})}
+              
+              />
+            </div>
+            <div>
+              <input type="url" placeholder='Enter the product image url' id='image' className='border w-full mb-4 p-2 rounded'
+              value={newProduct.image}
+              onChange={(e) => setNewProduct ({...newProduct, image: e.target.value})}
+              
+              />
+            </div>
+            
+            <div>
+              <input type="number" placeholder='Enter the product Stock' id='stock' className='border w-full mb-4 p-2 rounded' min={0}
+              value={newProduct.stock}
+              onChange={(e) => setNewProduct ({...newProduct, stock: e.target.value})}
+              
+              />
+            </div>
+            <textarea name="description" className='w-full border  border-gray-300' placeholder='Enter the product description' rows="5" id='description' 
+            value={newProduct.description}
+            onChange={(e) => setNewProduct ({...newProduct, description: e.target.value})}
+
+            >
+
+            </textarea>
+
+            <div className='flex justify-center mt-5'>
+              <button className='bg-blue-500 text-white px-4 py-2 cursor-pointer rounded shadow-amber-200 hover:bg-blue-600'>Create Product</button>
+            </div>  
+
+
+          </form>
+
+
+        </div>
+
+
+      </div>
+    </>
   )
 }
 
