9db5b3e2a32728dbce7a35a24f64e54c8d573af4
Tiisu <tiisusharif@gmail.com>
2025-07-27 21:54:52 +0000

    woring on the home page

--- Chat-App/frontend/src/pages/HomePage.jsx
+++ Chat-App/frontend/src/pages/HomePage.jsx
@@ -48,9 +48,7 @@ function HomePage() {
             <Card  />
             <Card  />
             <Card  />
-            <Card  />
-            <Card  />
-            <Card  />
+           
         </div>
       </div>
 
