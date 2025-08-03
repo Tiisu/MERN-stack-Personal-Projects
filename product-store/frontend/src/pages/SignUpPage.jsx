c686bcee1255335416e6af41f09bc1f1c00e363f
Tiisu <tiisusharif@gmail.com>
2025-08-03 20:34:47 +0000

    added some fields to the signup page

--- product-store/frontend/src/pages/SignUpPage.jsx
+++ product-store/frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <>
+    <div>
+        <h1>Create an Accoutn</h1>
+        <div>
+            <form className='flex flex-col gap-3 aligcn-center justify-center'>
+                <label htmlFor="">Name:
+                <input type="text" placeholder='Enter your name' className='border p-2 rounded' />
+                </label>
+                <label htmlFor="">Email:
+                <input type="email" placeholder='Enter your email' className='border p-2 rounded' />
+                </label>
+                <label htmlFor="">Password:
+                <input type="password" placeholder='Enter your password' className='border p-2 rounded' />
+                </label>
+                <label htmlFor="">Confirm Password:
+                <input type="password" placeholder='Confirm your password' className='border p-2 rounded' />
+                </label>
+                <button type='submit' className='bg-blue-500 text-white p-2 rounded'>Sign Up</button>
+            </form>
+        </div>
+    </div>
+
+    
+    </>
   )
 }
 
