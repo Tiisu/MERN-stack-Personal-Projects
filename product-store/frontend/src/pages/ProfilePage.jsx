0ac367c8e05eeec2c625afd4ec0f963e181f91ae
Tiisu <tiisusharif@gmail.com>
2025-08-10 23:21:30 +0000

    working on the profile page

--- product-store/frontend/src/pages/ProfilePage.jsx
+++ product-store/frontend/src/pages/ProfilePage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div>
+      <h1>MY profile</h1>
+      <p>Welcome to your profile page!</p>
+      <p>Here you can view and edit your personal information.</p>
+      <p>Feel free to update your details as needed.</p>
+      <p>Thank you for being a valued user!</p>
+      <p>Enjoy your experience on our platform!</p> 
+    </div>
   )
 }
 
