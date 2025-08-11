82d4b36011a39c83e073a5c279325acf9d9a8fb4
Tiisu <tiisusharif@gmail.com>
2025-08-11 23:18:53 +0000

    added some routes

--- product-store/frontend/src/App.jsx
+++ product-store/frontend/src/App.jsx
@@ -17,6 +17,11 @@ function App() {
         {/* Define your routes here */}
         < Route path="/" element={<HomePage />} />
         < Route path="create-product" element={<CreatePage />} />
+        < Route path="profile" element={<ProfilePage />} />
+        <Toaster />
+        {/* Add more routes as needed */}
+        < Route path="*" element={<HomePage />} />
+        < Route path="login" element={<SignUpPage />} />
         < Route path="signup" element={<SignUpPage />} />
         
       </Routes>
