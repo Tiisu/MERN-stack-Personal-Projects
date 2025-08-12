9f9e81f2c01e97eb92c70b322476ee540b24c62a
Tiisu <tiisusharif@gmail.com>
2025-08-12 22:48:33 +0000

    the profile

--- product-store/frontend/src/pages/ProfilePage.jsx
+++ product-store/frontend/src/pages/ProfilePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
+
     <div>
       <h1>MY profile</h1>
       <p>Welcome to your profile page!</p>
@@ -9,6 +10,12 @@ const ProfilePage = () => {
       <p>Feel free to update your details as needed.</p>
       <p>Thank you for being a valued user!</p>
       <p>Enjoy your experience on our platform!</p> 
+    <div>
+      <form action="">
+        <label htmlFor="">Name:</label>
+      </form>
+    </div>
+
     </div>
   )
 }
